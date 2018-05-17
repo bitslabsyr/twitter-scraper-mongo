9bea01f95d48afb12ef805c8a145c84b57d4f65b
stanupab <stanupab@syr.edu>
2018-05-17 09:03:01 -0400

    Rename main_dev.py to main.dev

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import sys
+import os
+       
+if not os.path.exists('./logs'):
+    os.makedirs('./logs')
+         
+import config as cfg
+from timeline_dev import run_timeline
 
-from db import init
-from timeline import run_timeline
-
-AUTH = {
-    'consumer_key': 'Gg7eA8iADELAcZMBdyPONRT6S',
-    'consumer_secret': 'gUC42t7oZKzapXfkw6jZeXqP4OLYm5KpgR9DrwElbR4XNG7fsA',
-    'access_token': '260546258-Xc9X55LLe0HAk7rD3oJXH46MU4yWJF1xRwao1h5k',
-    'access_token_secret': 'fYCEAG2RzWIzDaklHv9rgpTs0CNAXKvPA7xkpkw5XjoRe'
-}
 
 def main(method):
-    methods = ['timeline', 'init']
+    methods = ['timeline', 'reply', 'stat']
     method = method.strip('--')
 
     if method not in methods:
-        print 'ERROR: Invalid method. Please include a valid method.'
+        print('ERROR: Invalid method. Please include a valid method.')
         sys.exit(1)
-
-    if method == 'init':
-        init()
     elif method == 'timeline':
-        run_timeline(AUTH)
+        run_timeline(cfg.AUTH)
+    
 
 if __name__ == '__main__':
+    # Output Folders Handling #
+    if not os.path.exists('./rawdata/original'):
+        os.makedirs('./rawdata/original')
+    if not os.path.exists('./rawdata/reply'):
+        os.makedirs('./rawdata/reply')
+    
     method = sys.argv[1]
     main(method)
