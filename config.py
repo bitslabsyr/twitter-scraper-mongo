00ade36d4f7f8351f548c8b3de2c4d8a64d2d351
Alexander O. Smith <12877089+alexanderosmith@users.noreply.github.com>
2020-03-30 01:38:32 -0400

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from datetime import  datetime
 # auth: True if MongoDB authentication is enabled. If True, username and password must be given
 MONGO_ACCOUNT = {'address': 'localhost',
                  'auth': True,
-                 'username': 'bitslab',
-                 'password': '0rang3!'}
+                 'username': username,
+                 'password': passwd}
 # Format: date(YYYY, M, D, Hour, Min). If no date restriction desired, replace with False
 COLLECT_FROM = datetime(2017, 1, 1, 0, 0)
